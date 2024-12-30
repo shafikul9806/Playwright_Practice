8eb550a348425bb35850c8ba7ae08d6fd7fee789
shafikul9806 <shafikuloves@gmail.com>
2024-12-30 20:59:12 +0600

    example

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,12 +1,4 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
